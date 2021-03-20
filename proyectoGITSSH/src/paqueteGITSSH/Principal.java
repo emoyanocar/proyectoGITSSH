5e24a65920bbbb52b52971d1a469cf31f2fd0600
emoyanocar <42063679+emoyanocar@users.noreply.github.com>
2021-03-20 18:51:43 +0100

    Update Principal.java

--- proyectoGITSSH/src/paqueteGITSSH/Principal.java
+++ proyectoGITSSH/src/paqueteGITSSH/Principal.java
@@ -4,11 +4,13 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		syso
+		System.out.println("No sé qué está pasando"
 		System.out.println("Adiós Mundo Cruel");
 		//He dicho que el comentario se queda
 		//Entre bomberos no nos pisemos la manguera
 		//hola
+		
+		
 	}
 
 }
