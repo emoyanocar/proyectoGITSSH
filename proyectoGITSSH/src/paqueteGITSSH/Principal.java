062ba86b6ebd0cf5e60af7b6af6ee3315d0a1bc5
emoyanocar <emoyanocar@portatilEmC>
2021-03-20 18:46:29 +0100

    asdfasdf

--- proyectoGITSSH/src/paqueteGITSSH/Principal.java
+++ proyectoGITSSH/src/paqueteGITSSH/Principal.java
@@ -4,9 +4,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		syso
 		System.out.println("Adiós Mundo Cruel");
 		//He dicho que el comentario se queda
 		//Entre bomberos no nos pisemos la manguera
+		//hola
 	}
 
 }
