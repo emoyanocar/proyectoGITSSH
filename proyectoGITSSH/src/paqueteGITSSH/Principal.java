c73713b7967072aff2404845ed8c2284d4f79f01
emoyanocar <emoyanocar@portatilEmC>
2021-03-20 18:27:36 +0100

    Recommit

--- proyectoGITSSH/src/paqueteGITSSH/Principal.java
+++ proyectoGITSSH/src/paqueteGITSSH/Principal.java
@@ -5,7 +5,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Adiós Mundo Cruel");
-		//comentario desde github
 	}
 
 }
